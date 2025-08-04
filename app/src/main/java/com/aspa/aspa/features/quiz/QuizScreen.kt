6696d151f0fa86cec50c8779b8d5ac44bc8da224
ChoiHyeonSu0 <choi1211012@gmail.com>
2025-08-04 15:28:49 +0900

    퀴즈 목록 페이지 UI

--- app/src/main/java/com/aspa/aspa/features/quiz/QuizScreen.kt
+++ app/src/main/java/com/aspa/aspa/features/quiz/QuizScreen.kt
@@ -1,4 +1,331 @@
 package com.aspa.aspa.features.quiz
 
-class QuizScreen {
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QuizScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Aspa") }
+            )
+        },
+        content = { padding ->
+            Column(
+                modifier = Modifier.fillMaxSize().padding(padding).padding(horizontal = 8.dp)
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    text = "퀴즈",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                )
+
+                Text(
+                    text = "학습한 내용을 확인해 보세요.",
+                    color = Color.Gray
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    // modifier = Modifier.size(width = 400.dp, height = 120.dp),
+                    border = BorderStroke(1.dp, Color.Gray),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surface
+                    ),
+                    content = {
+                        Column(
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Text(
+                                text = "React 완전 정복",
+                            )
+                            
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(
+                                    text = "React 기초부터!",
+                                    color = Color.Gray
+                                )
+
+                                Card {
+                                    Text(
+                                        text = "2/6",
+                                        modifier = Modifier.padding(6.dp)
+                                    )
+                                }
+                            }
+
+                            Box(modifier = Modifier.height(20.dp))
+
+                            Row (
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text("전체 진도")
+
+                                Text("33%")
+                            }
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                            LinearProgressIndicator(
+                                progress = { 33f },
+                                modifier = Modifier.fillMaxWidth(),
+
+
+                            )
+
+                        }
+
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    border = BorderStroke(1.dp, Color.Gray),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surface
+                    ),
+                ) {
+                    Row(
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Favorite,
+                            contentDescription = null
+                        )
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text("JavaScript 기초")
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    border = BorderStroke(1.dp, Color.Gray),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surface
+                    ),
+                ) {
+                    Row(
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.FavoriteBorder,
+                            contentDescription = null
+                        )
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text("JavaScript 기초")
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    // modifier = Modifier.size(width = 400.dp, height = 120.dp),
+                    border = BorderStroke(1.dp, Color.Gray),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surface
+                    ),
+                    content = {
+                        Column(
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Text(
+                                text = "React 완전 정복",
+                            )
+
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(
+                                    text = "React 기초부터!",
+                                    color = Color.Gray
+                                )
+                            }
+
+                            Box(modifier = Modifier.height(20.dp))
+
+                            Row (
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Row (
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Card {
+                                        Text(
+                                            text = "React",
+                                            modifier = Modifier.padding(6.dp)
+                                        )
+                                    }
+                                    Spacer(modifier = Modifier.width(24.dp))
+
+                                    Text(
+                                        "3문제",
+                                        color = Color.Gray
+                                    )
+                                }
+
+
+                                Text(
+                                    text = "시작하기",
+                                    textDecoration = TextDecoration.Underline
+                                )
+
+                            }
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                        }
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    border = BorderStroke(1.dp, Color.Gray),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surface
+                    ),
+                    content = {
+                        Column(
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Text(
+                                text = "React 완전 정복",
+                            )
+
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(
+                                    text = "React 기초부터!",
+                                    color = Color.Gray
+                                )
+
+                                Card {
+                                    Row(
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                        Icon(
+                                            imageVector = Icons.Default.Favorite,
+                                            contentDescription = null
+                                        )
+
+                                        Text(
+                                            text = "80점",
+                                            modifier = Modifier.padding(6.dp)
+                                        )
+                                    }
+
+                                }
+                            }
+
+                            Box(modifier = Modifier.height(20.dp))
+
+                            Row (
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Row (
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Card {
+                                        Text(
+                                            text = "React",
+                                            modifier = Modifier.padding(6.dp)
+                                        )
+                                    }
+                                    Spacer(modifier = Modifier.width(24.dp))
+
+                                    Text(
+                                        "3문제",
+                                        color = Color.Gray
+                                    )
+                                }
+
+
+
+                                Text(
+                                    text = "2025. 8. 1.",
+
+                                )
+
+                            }
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                        }
+                    }
+                )
+
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QuizScreenPreview() {
+    QuizScreen()
 }
