45dfb3f3f327bd6a758b145c06668fdf4b92a6e1
ChoiHyeonSu0 <choi1211012@gmail.com>
2025-08-05 13:23:46 +0900

    더미 데이터 커밋 누락 추가

---
+++ app/src/main/java/com/aspa/aspa/features/quiz/DummySection.kt
@@ -0,0 +1,126 @@
+package com.aspa.aspa.features.quiz
+
+import com.aspa.aspa.model.Quiz
+import com.aspa.aspa.model.Roadmap
+import com.aspa.aspa.model.Section
+
+class DummySection {
+
+    companion object {
+        val dummyRoadmapList = listOf(
+            Roadmap(
+                title = "React 완전 정복",
+                description = "React 기초부터!",
+                completedSection = 2,
+                allSection = 6,
+                sections = listOf(
+                    Section(
+                        title = "JavaScript 기초",
+                        description = "description",
+                        concept = "concept",
+                        duration = "4~6주",
+                        status = true,
+                        quiz = Quiz(
+                            question = "TODO()",
+                            choices = listOf("TODO1()", "answer", "TODO2()", "TODO3()"),
+                            answer = "answer",
+                            chosen = "answer",
+                            description = ""
+                        )
+                    ),
+                    Section(
+                        title = "JavaScript 핵심",
+                        description = "description",
+                        concept = "concept",
+                        duration = "4~6주",
+                        status = false,
+                        quiz = Quiz(
+                            question = "TODO()",
+                            choices = listOf("TODO1()", "answer", "TODO2()", "TODO3()"),
+                            answer = "answer",
+                            chosen = "answer",
+                            description = ""
+                        )
+                    ),
+
+                    )
+            ),
+
+            Roadmap(
+                title = "React 완전 정복",
+                description = "React 기초부터!",
+                completedSection = 0,
+                allSection = 6,
+                sections = listOf(
+                    Section(
+                        title = "JavaScript 기초",
+                        description = "description",
+                        concept = "concept",
+                        duration = "4~6주",
+                        status = false,
+                        quiz = Quiz(
+                            question = "TODO()",
+                            choices = listOf("TODO1()", "answer", "TODO2()", "TODO3()"),
+                            answer = "answer",
+                            chosen = "answer",
+                            description = ""
+                        )
+                    ),
+                    Section(
+                        title = "JavaScript 핵심",
+                        description = "description",
+                        concept = "concept",
+                        duration = "4~6주",
+                        status = false,
+                        quiz = Quiz(
+                            question = "TODO()",
+                            choices = listOf("TODO1()", "answer", "TODO2()", "TODO3()"),
+                            answer = "answer",
+                            chosen = "answer",
+                            description = ""
+                        )
+                    ),
+                )
+            ),
+            Roadmap(
+                title = "React 완전 정복",
+                description = "React 기초부터!",
+                completedSection = 6,
+                allSection = 6,
+                sections = listOf(
+                    Section(
+                        title = "JavaScript 기초",
+                        description = "description",
+                        concept = "concept",
+                        duration = "4~6주",
+                        status = true,
+                        quiz = Quiz(
+                            question = "TODO()",
+                            choices = listOf("TODO1()", "answer", "TODO2()", "TODO3()"),
+                            answer = "answer",
+                            chosen = "answer",
+                            description = ""
+                        )
+                    ),
+                    Section(
+                        title = "JavaScript 핵심",
+                        description = "description",
+                        concept = "concept",
+                        duration = "4~6주",
+                        status = true,
+                        quiz = Quiz(
+                            question = "TODO()",
+                            choices = listOf("TODO1()", "answer", "TODO2()", "TODO3()"),
+                            answer = "answer",
+                            chosen = "answer",
+                            description = ""
+                        )
+                    ),
+                )
+            )
+        )
+    }
+
+
+
+}
