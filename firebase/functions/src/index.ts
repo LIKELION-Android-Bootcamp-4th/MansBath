235a7d5e37ff4bb48c4f66e2e889d17f0f78bf24
HyunjeLee <cyeclamen07@gmail.com>
2025-08-10 19:53:07 +0900

    feat(naver-login sdk): add functions `loginWithNaver`
    - using onCall()

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,6 +4,8 @@ import {initializeApp} from "firebase-admin/app";
 import {authApp} from "./auth/auth";
 import {questionApp} from "./question/question";
 import {generateRoadmap} from "./roadmap/roadmap";
+import {loginWithNaver} from "./auth/naver";
+
 
 // Firebase Admin SDK 초기화
 initializeApp();
@@ -14,3 +16,5 @@ initializeApp();
 export const auth = onRequest({region: "asia-northeast3"}, authApp);
 export const question = onRequest({region: "asia-northeast3"}, questionApp);
 export const roadmap = onRequest({region: "asia-northeast3"}, generateRoadmap);
+
+export {loginWithNaver};
