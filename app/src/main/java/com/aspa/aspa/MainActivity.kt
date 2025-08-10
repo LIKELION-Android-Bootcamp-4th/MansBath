0333d79df708b8bfc3e7581ac4d2565d501c451d
HyunjeLee <cyeclamen07@gmail.com>
2025-08-10 16:24:12 +0900

    feat(naver-login sdk): add naver social login

--- app/src/main/java/com/aspa/aspa/MainActivity.kt
+++ app/src/main/java/com/aspa/aspa/MainActivity.kt
@@ -1,38 +1,43 @@
 package com.aspa.aspa
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.aspa.aspa.ui.theme.AspaTheme
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.OAuthProvider
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.OAuthCredential
+import com.navercorp.nid.NaverIdLoginSDK
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        initNaverLoginSDK(this)
+
         setContent {
+            val context = LocalContext.current
+            val launcher = rememberNaverLoginLauncher()
+
             Column {
                 Box(modifier = Modifier.height(200.dp)) {}
-                Button(
-                    onClick = { loginWithNaver(this@MainActivity) }
-                ) {
-                    Text("naver login")
+                Button(onClick = {
+                    NaverIdLoginSDK.authenticate(context, launcher)
+                }) {
+                    Text("네이버로 로그인")
                 }
             }
 
@@ -40,96 +45,34 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-fun loginWithNaver(activity: Activity) {
-    val providerBuilder = OAuthProvider.newBuilder("oidc.naver").apply {
-        // 네이버 OIDC가 지원하는 스코프
-        scopes = listOf("openid", "profile")
-    }
-    val firebaseAuth = FirebaseAuth.getInstance()
+fun initNaverLoginSDK(activity: Activity) {
 
-
-    val pendingResultTask = firebaseAuth.pendingAuthResult
-    if (pendingResultTask != null) {
-        // There's something already here! Finish the sign-in for your user.
-        pendingResultTask
-            .addOnSuccessListener { result ->
-                // User is signed in.
-                // IdP data available in
-                // authResult.getAdditionalUserInfo().getProfile().
-                // The OAuth access token can also be retrieved:
-                // ((OAuthCredential)authResult.getCredential()).getAccessToken().
-                // The OAuth secret can be retrieved by calling:
-                // ((OAuthCredential)authResult.getCredential()).getSecret().
-                handleAuthSuccess(result)
-            }
-            .addOnFailureListener { e ->
-                // Handle failure.
-                handleAuthFailure(e)
-            }
-    } else {
-        // There's no pending result so you need to start the sign-in flow.
-        // See below.
-
-        firebaseAuth
-            .startActivityForSignInWithProvider(activity, providerBuilder.build())
-            .addOnSuccessListener { result ->
-                // User is signed in.
-                // IdP data available in
-                // authResult.getAdditionalUserInfo().getProfile().
-                // The OAuth access token can also be retrieved:
-                // ((OAuthCredential)authResult.getCredential()).getAccessToken().
-                // The OAuth secret can be retrieved by calling:
-                // ((OAuthCredential)authResult.getCredential()).getSecret().
-                handleAuthSuccess(result)
-            }
-            .addOnFailureListener { e ->
-                // Handle failure.
-                handleAuthFailure(e)
-            }
-    }
-}
-
-private fun handleAuthSuccess(result: AuthResult) {
-    val user = result.user
-    val profileMap = result.additionalUserInfo?.profile // IdP 원본 프로필(Map)
-    val cred = result.credential as? OAuthCredential
-    val accessToken = cred?.accessToken
-    val idToken = cred?.idToken
-
-    Log.d("AUTH_SUCCESS", "user: $user")
-    Log.d("AUTH_SUCCESS", "profileMap: $profileMap")
-    Log.d("AUTH_SUCCESS", "accessToken: $accessToken")
-    Log.d("AUTH_SUCCESS", "idToken: $idToken")
-}
-
-private fun handleAuthFailure(e: Exception) {
-    val stack = e.stackTrace.joinToString("\n") { element ->
-        "    at $element"
-    }
-    val fe = e as? com.google.firebase.auth.FirebaseAuthException
-    Log.e(
-        "AUTH_ERROR",
-        "code=${fe?.errorCode}\n" +
-                "msg: ${fe?.message}\n" +
-                "cause: ${e.cause}\n" +
-                "localizedMessage: ${e.localizedMessage}\n" +
-                Log.getStackTraceString(e)
+    NaverIdLoginSDK.initialize(
+        context = activity.applicationContext,
+        clientId = BuildConfig.NAVER_CLIENT_ID,
+        clientSecret = BuildConfig.NAVER_CLIENT_SECRET,
+        clientName = BuildConfig.APPLICATION_ID
     )
 }
 
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun rememberNaverLoginLauncher(): ActivityResultLauncher<Intent> {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AspaTheme {
-        Greeting("Android")
+    return rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val accessToken = NaverIdLoginSDK.getAccessToken()
+
+            Log.d("NAVER_LOGIN", "✅ 로그인 성공")
+            Log.d("NAVER_LOGIN", "AccessToken: $accessToken")
+        } else {
+            val code = NaverIdLoginSDK.getLastErrorCode().code
+            val desc = NaverIdLoginSDK.getLastErrorDescription()
+
+            Log.e("NAVER_LOGIN", "❌ 로그인 실패")
+            Log.e("NAVER_LOGIN", "Error Code: $code")
+            Log.e("NAVER_LOGIN", "Error Desc: $desc")
+        }
     }
 }
