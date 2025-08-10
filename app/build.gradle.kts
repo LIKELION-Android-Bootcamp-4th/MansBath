acfacc20aa188cf4c2d7538c8b6854136c02178c
ChoiHyeonSu0 <choi1211012@gmail.com>
2025-08-10 10:20:28 +0900

    fix: 카카오 계정 로그인이 정상 작동하지 않는 문제 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "KAKAO_NATIVE_APP_KEY", myProperties.getProperty("kakaoNativeAppKey"))
-        manifestPlaceholders["kakaoNativeAppKey"] = myProperties.getProperty("kakaoNativeAppKey")
+        manifestPlaceholders["kakaoNativeAppKeyforManifest"] = myProperties.getProperty("kakaoNativeAppKeyforManifest")
     }
 
     buildTypes {
