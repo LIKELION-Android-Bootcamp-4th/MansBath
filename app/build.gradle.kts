4f9b5ed284b8a3cc99d3ac7e8c496691b7727872
HyunjeLee <cyeclamen07@gmail.com>
2025-08-10 23:14:01 +0900

    feat: add dependency navigation-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(libs.compose.material.icons.extended)
+
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
