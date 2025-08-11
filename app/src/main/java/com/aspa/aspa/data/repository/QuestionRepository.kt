3e7915c289e579dc888fe8cad2b63ff736b2d9e2
haebun <mediafactory1279@gmail.com>
2025-08-11 14:29:36 +0900

    comment : 주석 추가

--- app/src/main/java/com/aspa/aspa/data/repository/QuestionRepository.kt
+++ app/src/main/java/com/aspa/aspa/data/repository/QuestionRepository.kt
@@ -1,11 +1,15 @@
 package com.aspa.aspa.data.repository
 
 import com.aspa.aspa.data.remote.QuestionRemoteDataSource
-import com.aspa.aspa.data.remote.dto.QuestionResponseDto // DTO import 추가
+import com.aspa.aspa.data.remote.dto.QuestionResponseDto
 
 class QuestionRepository {
     private val questionRemoteDataSource = QuestionRemoteDataSource()
 
+    /**
+     * 질문 DTO 처리
+     * 추후 onCall 방식 처리 시 기본 골자
+     */
     suspend fun sendQuestion(question: String, questionId: String?): QuestionResponseDto? {
         return try {
             questionRemoteDataSource.sendQuestion(question, questionId)
@@ -13,4 +17,5 @@ class QuestionRepository {
             throw e
         }
     }
+
 }
