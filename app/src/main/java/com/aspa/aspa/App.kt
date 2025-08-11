7ef7450287e79ffbad8c28faf8cb6d960144dc07
rlaaudgjs2 <68803644+rlaaudgjs2@users.noreply.github.com>
2025-08-11 23:48:01 +0900

    설명 추가

--- app/src/main/java/com/aspa/aspa/App.kt
+++ app/src/main/java/com/aspa/aspa/App.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() // 전 주입의 시작점. 무조건 있어야함. hilt에 관련된 설치점이라 생각하면 됨.
