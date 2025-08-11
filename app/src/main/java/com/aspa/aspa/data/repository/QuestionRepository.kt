c127437824ad4e62d593b6e034d70c058bab3984
haebun <mediafactory1279@gmail.com>
2025-08-11 14:26:44 +0900

    feat : question function 응답 배치
    기존의 리스너 대신 직접 응답 결과를 바로 화면에 배치하도록 수정

--- app/src/main/java/com/aspa/aspa/data/repository/QuestionRepository.kt
+++ app/src/main/java/com/aspa/aspa/data/repository/QuestionRepository.kt
@@ -1,14 +1,14 @@
 package com.aspa.aspa.data.repository
 
 import com.aspa.aspa.data.remote.QuestionRemoteDataSource
+import com.aspa.aspa.data.remote.dto.QuestionResponseDto // DTO import 추가
 
 class QuestionRepository {
     private val questionRemoteDataSource = QuestionRemoteDataSource()
 
-    suspend fun sendQuestion(question: String, questionId: String?): String? {
+    suspend fun sendQuestion(question: String, questionId: String?): QuestionResponseDto? {
         return try {
-            val response = questionRemoteDataSource.sendQuestion(question, questionId)
-            response?.questionId
+            questionRemoteDataSource.sendQuestion(question, questionId)
         } catch (e: Exception) {
             throw e
         }
