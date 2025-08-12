8961c9e0df98ac2cba8587da907be5c395126036
Haebun <mediafactory1279@gmail.com>
2025-08-12 09:53:16 +0900

    fix : onCall 라우팅 변경

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,7 +2,7 @@ import {onRequest} from "firebase-functions/v2/https";
 import {initializeApp} from "firebase-admin/app";
 
 import {authApp} from "./auth/auth";
-import {questionApp} from "./question/question";
+import {question} from "./question/question";
 import {generateRoadmap} from "./roadmap/roadmap";
 
 // Firebase Admin SDK 초기화
@@ -12,5 +12,6 @@ initializeApp();
 // ✨ 분리된 함수 내보내기
 // =================================================================
 export const auth = onRequest({region: "asia-northeast3"}, authApp);
-export const question = onRequest({region: "asia-northeast3"}, questionApp);
 export const roadmap = onRequest({region: "asia-northeast3"}, generateRoadmap);
+
+export {question};
