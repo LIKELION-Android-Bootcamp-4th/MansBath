56ea045d7a7b3ccc0a4d9c1632bf015fb003bdbc
HyunjeLee <cyeclamen07@gmail.com>
2025-08-13 16:43:45 +0900

    refactor(question): using onCall from OnRequest

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,7 +2,7 @@ import {onRequest} from "firebase-functions/v2/https";
 import {initializeApp} from "firebase-admin/app";
 
 import {authApp} from "./auth/auth";
-import {questionApp} from "./question/question";
+import {question} from "./question/question";
 import {generateRoadmap} from "./roadmap/roadmap";
 import {quizApp} from "./quiz/quiz";
 import {loginWithNaver} from "./auth/naver";
@@ -15,8 +15,8 @@ initializeApp();
 // ✨ 분리된 함수 내보내기
 // =================================================================
 export const auth = onRequest({region: "asia-northeast3"}, authApp);
-export const question = onRequest({region: "asia-northeast3"}, questionApp);
 export const roadmap = onRequest({region: "asia-northeast3"}, generateRoadmap);
 export const quiz = onRequest({region: "asia-northeast3"}, quizApp);
 
+export {question};
 export {loginWithNaver};
