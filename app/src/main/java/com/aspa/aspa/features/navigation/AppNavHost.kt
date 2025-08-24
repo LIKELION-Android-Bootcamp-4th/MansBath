229a0c1975ee90256c2cd8930fb640519cf9e3ba
HyunjeLee <cyeclamen07@gmail.com>
2025-08-24 23:31:12 +0900

    feat: 자동 로그인 기능 추가
    firebase sdk를 이용해 캐싱된 토큰이 남아있다면 바로 main으로, 남아있지 않다면 login으로 이동
    
    해당 로직을 위한 SplashScreen.kt 추가

--- app/src/main/java/com/aspa/aspa/features/navigation/AppNavHost.kt
+++ app/src/main/java/com/aspa/aspa/features/navigation/AppNavHost.kt
@@ -4,7 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.aspa.aspa.features.login.navigation.LoginDestinations
+import com.aspa.aspa.SplashDestinations
+import com.aspa.aspa.SplashScreen
 import com.aspa.aspa.features.login.navigation.loginGraph
 import com.aspa.aspa.features.main.MainScreen
 import com.aspa.aspa.features.main.navigation.MainDestinations
@@ -13,13 +14,16 @@ import com.aspa.aspa.features.main.navigation.MainDestinations
 fun AppNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = LoginDestinations.LOGIN_GRAPH_ROUTE) {
+    NavHost(navController = navController, startDestination = SplashDestinations.SPLASH) {
 
         loginGraph(navController)
 
         composable(MainDestinations.MAIN) {
-            // navController를 전달하지 않음
             MainScreen(navController)
+        }
+
+        composable(SplashDestinations.SPLASH) {
+            SplashScreen(navController)
         }
     }
 }
