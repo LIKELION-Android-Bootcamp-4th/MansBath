56f190b78773c7bda07d7c324bcb4d0d10515626
ChoiHyeonSu0 <choi1211012@gmail.com>
2025-08-26 18:52:41 +0900

    fix: 한 유저에게 알림이 한번만 가게 수정

--- firebase/functions/src/fcm/fcm_service.ts
+++ firebase/functions/src/fcm/fcm_service.ts
@@ -26,7 +26,7 @@ async function checkQuizStatus() {
   try {
     // 1. 모든 유저 조회
     const usersSnapshot = await getFirestore().collection("users").get();
-    for (const userDoc of usersSnapshot.docs) {
+    userSearchLoop: for (const userDoc of usersSnapshot.docs) {
       console.log(`Searching user: ${userDoc.id}`);
       const quizzesSnapshot = await userDoc.ref.collection("quizzes").get();
       for (const roadmapDoc of quizzesSnapshot.docs) {
@@ -35,7 +35,9 @@ async function checkQuizStatus() {
           if (doc.data().status == false) {
             // 2. 해당 유저의 FCM 토큰 가져오기
             const token = userDoc.get("fcmToken");
-            if (!token) continue;
+            if (!token) {
+              continue userSearchLoop;
+            }
 
             // 3. 메시지 전송
             const message = {
@@ -48,7 +50,7 @@ async function checkQuizStatus() {
 
             await admin.messaging().send(message);
             console.log(`✅ Sent FCM to ${userDoc.id}`);
-            break;
+            continue userSearchLoop;
           }
         }
       }
