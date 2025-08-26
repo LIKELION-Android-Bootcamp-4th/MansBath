4cc1a35074051803493b4e44c9afb08eb2f76e5a
HyunjeLee <cyeclamen07@gmail.com>
2025-08-27 00:30:29 +0900

    chore: merge fix

--- app/src/main/java/com/aspa/aspa/MainActivity.kt
+++ app/src/main/java/com/aspa/aspa/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.compose.rememberNavController
 import com.aspa.aspa.features.navigation.AppNavigation
 import com.aspa.aspa.ui.theme.AspaTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,10 +16,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val navController = rememberNavController()
             AspaTheme {
                 AppNavigation()
-
             }
         }
     }
